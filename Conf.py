3c52b42e334c4904068c9fd3ae8c3b8e0161dd62
ating <yanbiaoamazing@gmail.com>
2017-04-19 09:57:56 +0800

    add waterhole config

--- Conf.py
+++ Conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """"
-    File name: Conf.py
+    File name: main2.py
     Author: amazing
     Date last modified: 3/28/2017
     Python Version: 3.5
@@ -8,7 +8,7 @@
 
 # HOST = "182.92.104.116"
 HOST = "182.92.104.116"
-PORT = 9999
+PORT = 8999
 
 TIME_SPAN = 20
 
@@ -17,9 +17,7 @@ ZEC = "ZEC"
 
 F2POOL = "f2pool"
 ETHFANS = "ethfans"
+WATERHOLE = "waterhole"
 
 COIN2INDEX = {ETH: 0, ZEC: 1}
-POOL2INDEX = {F2POOL: 0, ETHFANS: 1}
-
-
-
+POOL2INDEX = {F2POOL: 0, ETHFANS: 1, WATERHOLE: 2}
